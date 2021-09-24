3a43956c90bcb58c2fb671cc531885027b546b76
sahilrajathakuria <35011987+sahilrajathakuria@users.noreply.github.com>
2021-09-24 21:52:30 +0500

    commit

--- APiPro/settings.py
+++ APiPro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'hw**bwzgk%tk377383%9&cs&i%i#86z1+yb()2-&^ihp_cmpz7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','fyp-project123.herokuapp.com']
 
 
 # Application definition
