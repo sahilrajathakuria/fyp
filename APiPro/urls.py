f826b5f9ffad933d4b24ab11b45e2fbd5b0670cc
sahilrajathakuria <35011987+sahilrajathakuria@users.noreply.github.com>
2021-09-25 18:15:09 +0500

    Update urls.py

--- APiPro/urls.py
+++ APiPro/urls.py
@@ -25,7 +25,7 @@ from django.views.static import serve
 from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("Home.urls")),
+    path('', include("APiPro.urls")),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
