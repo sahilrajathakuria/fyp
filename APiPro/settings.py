306209c1b6ea8a859931b4083dca8deb0d24aae6
sahilrajathakuria <35011987+sahilrajathakuria@users.noreply.github.com>
2021-09-25 18:11:53 +0500

    Update settings.py

--- APiPro/settings.py
+++ APiPro/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'hw**bwzgk%tk377383%9&cs&i%i#86z1+yb()2-&^ihp_cmpz7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','project-id-36.herokuapp.com']
 
